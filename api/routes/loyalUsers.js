81f876f4d11cbec26715828399e0aff0d9585726
Andrei Egorov <andreyegorov1992@gmail.com>
2018-10-01 08:11:59 -0700

    Create server, Add API Routes and Queries, Create Docker Container file

---
+++ api/routes/loyalUsers.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const ClickHouse = require('@apla/clickhouse');
+
+const options = {
+  host: '0.0.0.0',
+  port: 8123,
+  queryOptions: {
+    database: 'Users',
+  },
+};
+const ch = new ClickHouse(options);
+
+router.get('/', (req, res, next) => {
+  const device =
+    req.query.device === undefined
+      ? [0, 1, 2, 3, 4, 5]
+      : req.query.device.split(',');
+  const os =
+    req.query.os === undefined
+      ? [0, 1, 2, 3, 4, 5, 6]
+      : req.query.os.split(',');
+
+  const stream = ch.query(
+    `SELECT COUNT(*) FROM(SELECT COUNT(User) FROM newsusers8 WHERE Os IN (${os}) AND Device IN (${device}) GROUP BY User HAVING COUNT(User) > 10)`,
+  );
+
+  const rows = [];
+  stream.on('data', row => {
+    rows.push(row);
+  });
+  stream.on('end', () => {
+    res.status(200).json(rows);
+  });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+
+//for test query
+// const ClickHouse = require('@apla/clickhouse');
+// const options = {
+//   host: '0.0.0.0',
+//   port: 8123,
+//   queryOptions: {
+//     database: 'Users',
+//   },
+// };
+// const ch = new ClickHouse(options);
+
+const uniqueUsersRoutes = require('./api/routes/uniqueUsers');
+const loyalUsersRoutes = require('./api/routes/loyalUsers');
+
+app.use('/unique-users', uniqueUsersRoutes);
+app.use('/loyal-users', loyalUsersRoutes);
+
+//test query
+// app.use((req, res, next) => {
+//   const stream = ch.query('SELECT * FROM newsusers8 LIMIT 10');
+//   const rows = [];
+//   stream.on('data', row => {
+//     rows.push(row);
+//   });
+//   stream.on('end', () => {
+//     res.status(200).json(rows);
+//   });
+// });
+
+app.use((req, res, next) => {
+  const error = new Error(
+    'This url does not return any data, please type another url, Ex: http://localhost:3000/unique-users?device=1,2&os=1',
+  ); //Error is global object
+  error.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
